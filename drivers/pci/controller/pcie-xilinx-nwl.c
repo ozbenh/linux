d23baca291d3862c375692c4c0edd2062f800906
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2019-06-24 14:16:05 +1000

    PCI: controller: xilinx-nwl: Use pci_host_resource_survey()
    Use this instead of pci_assign_unassigned_bus_resources() as it is
    more appropriate for a host bridge.
    
    The result should be equivalent based on the default ARM policy.
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- drivers/pci/controller/pcie-xilinx-nwl.c
+++ drivers/pci/controller/pcie-xilinx-nwl.c
@@ -817,8 +817,6 @@ static int nwl_pcie_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
 	struct nwl_pcie *pcie;
-	struct pci_bus *bus;
-	struct pci_bus *child;
 	struct pci_host_bridge *bridge;
 	int err;
 	resource_size_t iobase = 0;
@@ -882,12 +880,8 @@ static int nwl_pcie_probe(struct platform_device *pdev)
 	if (err)
 		goto error;
 
-	bus = bridge->bus;
-
-	pci_assign_unassigned_bus_resources(bus);
-	list_for_each_entry(child, &bus->children, node)
-		pcie_bus_configure_settings(child);
-	pci_bus_add_devices(bus);
+	pci_host_resource_survey(bridge->bus);
+	pci_bus_add_devices(bridge->bus);
 	return 0;
 
 error:
